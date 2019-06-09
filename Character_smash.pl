4e226f495425cf4d75c3f746a8a9222ea7388748
luigitercero <luigitercero3@gmail.com>
2019-06-08 23:08:14 -0600

    agregando super poderes

--- Character_smash.pl
+++ Character_smash.pl
@@ -10,8 +10,8 @@ instancia_de(Fox,zorro) .
 instancia_de(Pikachu,rata) .
 instancia_de(Luigi,humano).
 instancia_de(Ness,humano) .
-subclase_de(Captain_Falcon,humano) .
-subclase_de(Jigglypuff,pelota) .
+instancia_de(Captain_Falcon,humano) .
+instancia_de(Jigglypuff,pelota) .
 
 subclase_de(Mario,super_mario) .
 subclase_de(Donkey_Kong,donkey_kong) .
@@ -27,3 +27,26 @@ subclase_de(Ness,earth_boulind) .
 subclase_de(Captain_Falcon,f-zero) .
 subclase_de(Jigglypuff,pokemo) .
 
+poder_de(Mario,neutral,bola_de_fuego)
+poder_de(Donkey_Kong,neutral,super_puño)
+poder_de(Link,neutral,bumerang)
+poder_de(Samus,neutral,bola_de_electricidad) .
+poder_de(yoshi,neutral,Convirte_en_huevo) .
+poder_de(Kirby,neutral,traga) .
+poder_de(Fox,neutral,dispara) .
+poder_de(Pikachu,neutral,trueno) .
+poder_de(Luigi,neutral,bola_de_fuego).
+poder_de(Ness,neutral,ike_fire) .
+poder_de(Captain_Falcon,neutral,super_puño) .
+poder_de(Jigglypuff,neutral,rodar) .
+
+neutral(bola_de_fuego,es_proyectil) .
+neutral(super_puño,no_es_proyectil) .
+neutral(bumerang,es_proyectil) .
+neutral(bola_de_electricidad,es_proyectil) .
+neutral(Convirte_en_huevo,no_es_proyectil) .
+neutral(traga,no_es_proyectil) .
+neutral(dispara,es_proyectil) .
+neutral(trueno,es_proyectil) .
+neutral(ike_fire,es_proyectil) .
+neutral(rodar,no_es_proyectil) .
