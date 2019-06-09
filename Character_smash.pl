b1da6e7fec74415012bde1b54ce9881b1e25f2cd
luigitercero <luigitercero3@gmail.com>
2019-06-08 22:39:03 -0600

    subclases

--- Character_smash.pl
+++ Character_smash.pl
@@ -1,13 +1,29 @@
-instancia_de(Mario,super_mario) .
-instancia_de(Donkey_Kong,donkey_kong) .
-instancia_de(Link,legend_of_zelda) .
-instancia_de(Samus,Metroid) .
-instancia_de(Dark_Samus,Metroid) .
-instancia_de(Yoshi,Yoshi) .
-instancia_de(Kirby,Kirby) .
-instancia_de(Fox,star_fox) .
-instancia_de(Pikachu,pokemo) .
-instancia_de(Luigi,super_mario) .
-instancia_de(Ness,earth_Boulind) .
-instancia_de(Captain_Falcon,f-zero) .
-instancia_de(Jigglypuff,pokemo) .
+
+instancia_de(Mario,humano).
+instancia_de(Donkey_Kong,mono).
+instancia_de(Link,humano) .
+instancia_de(Samus,humano) .
+instancia_de(Samus,metroid) .
+instancia_de(Yoshi,dinosaurio) .
+instancia_de(Kirby,pelota) .
+instancia_de(Fox,zorro) .
+instancia_de(Pikachu,rata) .
+instancia_de(Luigi,humano).
+instancia_de(Ness,humano) .
+subclase_de(Captain_Falcon,humano) .
+subclase_de(Jigglypuff,pelota) .
+
+subclase_de(Mario,super_mario) .
+subclase_de(Donkey_Kong,donkey_kong) .
+subclase_de(Link,legend_of_zelda) .
+subclase_de(Samus,Metroid) .
+subclase_de(Dark_Samus,Metroid) .
+subclase_de(Yoshi,Yoshi) .
+subclase_de(Kirby,Kirby) .
+subclase_de(Fox,star_fox) .
+subclase_de(Pikachu,pokemo) .
+subclase_de(Luigi,super_mario) .
+subclase_de(Ness,earth_boulind) .
+subclase_de(Captain_Falcon,f-zero) .
+subclase_de(Jigglypuff,pokemo) .
+
