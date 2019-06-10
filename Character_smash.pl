0e156970af5e0c405ffc354d43339b4f00e2e022
luigitercero <luigitercero3@gmail.com>
2019-06-10 07:52:47 -0600

    consultas

--- Character_smash.pl
+++ Character_smash.pl
@@ -2,8 +2,8 @@
 instancia_de(mario,humano).
 instancia_de(donkey_kong,mono).
 instancia_de(link,humano) .
-instancia_de(smus,humano) .
-instancia_de(smus,metroid) .
+instancia_de(samus,humano) .
+instancia_de(samus,metroid) .
 instancia_de(yoshi,dinosaurio) .
 instancia_de(kirby,pelota) .
 instancia_de(fox,zorro) .
@@ -13,59 +13,59 @@ instancia_de(ness,humano) .
 instancia_de(captain_falcon,humano) .
 instancia_de(jigglypuff,pelota) .
 
-tiene_propiedad_de(mario,tiene,narizota) .
-tiene_propiedad_de(luigi,tiene,narizota) .
-tiene_propiedad_de(donkey_kong,tiene,manotas) .
-tiene_propiedad_de(link,tiene,espada) .
-tiene_propiedad_de(link,tiene,escudo) .
-tiene_propiedad_de(smus,tiene,traje) .
-tiene_propiedad_de(yoshi,tiene,zapatos) .
-tiene_propiedad_de(ness,tiene,niño) .
-tiene_propiedad_de(captain_falcon,tiene, vehiculo) .
+tiene_propiedad(mario,tiene,narizota) .
+tiene_propiedad(luigi,tiene,narizota) .
+tiene_propiedad(donkey_kong,tiene,manotas) .
+tiene_propiedad(link,tiene,espada) .
+tiene_propiedad(link,tiene,escudo) .
+tiene_propiedad(samus,tiene,traje) .
+tiene_propiedad(yoshi,tiene,zapatos) .
+tiene_propiedad(ness,tiene,niño) .
+tiene_propiedad(captain_falcon,tiene, vehiculo) .
 
 
-tiene_propiedad_de(mario,notiene,manotas) .
-tiene_propiedad_de(luigi,notiene,manotas) .
-tiene_propiedad_de(mario,notiene,vehiculo) .
-tiene_propiedad_de(luigi,notiene,vehiculo) .
-tiene_propiedad_de(mario,notiene,espada) .
-tiene_propiedad_de(luigi,notiene,escudo) .
-tiene_propiedad_de(luigi,notiene,traje) .
-tiene_propiedad_de(mario,notiene,traje) .
+tiene_propiedad(mario,notiene,manotas) .
+tiene_propiedad(luigi,notiene,manotas) .
+tiene_propiedad(mario,notiene,vehiculo) .
+tiene_propiedad(luigi,notiene,vehiculo) .
+tiene_propiedad(mario,notiene,espada) .
+tiene_propiedad(luigi,notiene,escudo) .
+tiene_propiedad(luigi,notiene,traje) .
+tiene_propiedad(mario,notiene,traje) .
 
-tiene_propiedad_de(donkey_kong,notiene,narizota) .
-tiene_propiedad_de(donkey_kong,notiene,vehiculo) .
-tiene_propiedad_de(donkey_kong,notiene,espada) .
-tiene_propiedad_de(donkey_kong,notiene,escudo) .
-tiene_propiedad_de(donkey_kong,notiene,traje) .
+tiene_propiedad(donkey_kong,notiene,narizota) .
+tiene_propiedad(donkey_kong,notiene,vehiculo) .
+tiene_propiedad(donkey_kong,notiene,espada) .
+tiene_propiedad(donkey_kong,notiene,escudo) .
+tiene_propiedad(donkey_kong,notiene,traje) .
 
-tiene_propiedad_de(link,notiene,narizota) .
-tiene_propiedad_de(link,notiene,vehiculo) .
-tiene_propiedad_de(link,notiene,manotas) .
-tiene_propiedad_de(link,notiene,traje) .
-
-
-tiene_propiedad_de(smus,notiene,traje) .
+tiene_propiedad(link,notiene,narizota) .
+tiene_propiedad(link,notiene,vehiculo) .
+tiene_propiedad(link,notiene,manotas) .
+tiene_propiedad(link,notiene,traje) .
 
 
-tiene_propiedad_de(smus,notiene,narizota) .
-tiene_propiedad_de(smus,notiene,vehiculo) .
-tiene_propiedad_de(smus,notiene,espada) .
-tiene_propiedad_de(smus,notiene,escudo) .
+tiene_propiedad(samus,notiene,traje) .
+
+
+tiene_propiedad(samus,notiene,narizota) .
+tiene_propiedad(samus,notiene,vehiculo) .
+tiene_propiedad(samus,notiene,espada) .
+tiene_propiedad(samus,notiene,escudo) .
 
 
 
-tiene_propiedad_de(yoshi,notiene,narizota) .
-tiene_propiedad_de(yoshi,notiene,vehiculo) .
-tiene_propiedad_de(yoshi,notiene,espada) .
-tiene_propiedad_de(yoshi,notiene,escudo) .
-tiene_propiedad_de(yoshi,notiene,traje) .
+tiene_propiedad(yoshi,notiene,narizota) .
+tiene_propiedad(yoshi,notiene,vehiculo) .
+tiene_propiedad(yoshi,notiene,espada) .
+tiene_propiedad(yoshi,notiene,escudo) .
+tiene_propiedad(yoshi,notiene,traje) .
 
-tiene_propiedad_de(ness,notiene,narizota) .
-tiene_propiedad_de(ness,notiene,vehiculo) .
-tiene_propiedad_de(ness,notiene,espada) .
-tiene_propiedad_de(ness,notiene,escudo) .
-tiene_propiedad_de(ness,notiene,traje) .
+tiene_propiedad(ness,notiene,narizota) .
+tiene_propiedad(ness,notiene,vehiculo) .
+tiene_propiedad(ness,notiene,espada) .
+tiene_propiedad(ness,notiene,escudo) .
+tiene_propiedad(ness,notiene,traje) .
 
 incompatible(tiene(X),notiene(X)).
 
@@ -73,37 +73,24 @@ incompatible(tiene(X),notiene(X)).
 subclase_de(mario,super_mario) .
 subclase_de(donkey_kong,donkey_kong) .
 subclase_de(link,legend_of_zelda) .
-subclase_de(smus,metroid) .
-subclase_de(dark_smus,metroid) .
+subclase_de(samus,metroid) .
+subclase_de(dark_samus,metroid) .
 subclase_de(yoshi,yoshi) .
 subclase_de(kirby,kirby) .
 subclase_de(fox,star_fox) .
-subclase_de(pikachu,pokemo) .
+subclase_de(pikachu,pokemon) .
 subclase_de(luigi,super_mario) .
 subclase_de(ness,earth_boulind) .
 subclase_de(captain_falcon,f-zero) .
-subclase_de(jigglypuff,pokemo) .
+subclase_de(jigglypuff,pokemon) .
 
-subclase_de(super_mario,smash) .
-subclase_de(donkey_kong,smash) .
-subclase_de(legend_of_zelda,smash) .
-subclase_de(metroid,smash) .
-subclase_de(metroid,smash) .
-subclase_de(yoshi,smash) .
-subclase_de(kirby,smash) .
-subclase_de(star_fox,smash) .
-subclase_de(pokemo,smash) .
-subclase_de(super_mario,smash) .
-subclase_de(earth_boulind,smash) .
-subclase_de(f-zero,smash) .
-subclase_de(pokemo,smash) .
 
 
 
 poder_de(mario,neutral,bola_de_fuego) .
 poder_de(donkey_kong,neutral,super_puño) .
 poder_de(link,neutral,bumerang) .
-poder_de(smus,neutral,bola_de_electricidad) .
+poder_de(samus,neutral,bola_de_electricidad) .
 poder_de(yoshi,neutral,convirte_en_huevo) .
 poder_de(kirby,neutral,traga) .
 poder_de(fox,neutral,dispara) .
@@ -123,4 +110,17 @@ neutral(dispara,es_proyectil) .
 neutral(trueno,es_proyectil) .
 neutral(ike_fire,es_proyectil) .
 neutral(rodar,no_es_proyectil) .
+
+es(Clase,Obj):- instancia_de(Obj,Clase).
+es(Clase,Obj):- instancia_de(Obj,Clasep),
+                subc(Clasep,Clase).
+
+subc(C1,C2):- subclase_de(C1,C2).
+subc(C1,C2):- subclase_de(C1,C3),
+              subc(C3,C2).
+propiedad(Obj,Prop):-
+tiene_propiedad(Clase,Fun,Arg),
+Prop =.. [Fun,Arg].
+
+
 
--- README.md
+++ README.md
@@ -15,5 +15,27 @@ swipl likes.pl
 ```
 halt .
 ```
+## comentario 
+
+```prolog
+%esto es un comentario en prolog
+```
+## Fuciones de Busqueda
+
+todos las clases y objetos al cosultar estan en minusculas y en mayusculas las variables
+
+```prolog
+% consulta que es mario
+es(X,mario) .
+```
+
+```
+% obtinene todas las razas ejem:humano, mono ,etc ..
+es(X,Y) .
+```
+**al precionar espacio para hacer la consulta debe precionar ";"  y luego enter para mostrar los demas atributos**
+
+
+
 
 
